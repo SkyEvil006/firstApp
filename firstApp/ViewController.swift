cf9f43104ea158b3025f32b220f0f25a22df26a8
SkyEvil006 <skyevil006@gmail.com>
2022-12-07 18:24:45 +0300

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Владислава Павлюченко on 07.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
